272a53435c647097ee13d0ea68f39cf54bee5e32
後藤圭登 <keito@gotoukeitounoMacBook-Pro.local>
2025-03-15 23:19:25 +0900

    first commit

---
+++ src/main/java/com/example/fuelmanager/FuelmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.fuelmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fuelmanager/FuelmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fuelmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
